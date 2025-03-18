bf1549cc2f0ef72b3267a8f522163d7b241c730a
ricaramx77 <ricaramx77@outlook.com>
2025-03-17 18:09:43 -0600

    ok

--- components/main-header.js
+++ components/main-header.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import logoImg from '@/assets/logo.png';
@@ -7,7 +8,7 @@ export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
