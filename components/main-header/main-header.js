08059115bd0d99f88bdd2a3893633a54510ab59f
ricaramx77 <ricaramx77@outlook.com>
2025-03-19 18:33:59 -0600

    ok

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
 import MainHeaderBackground from './main-header-background';
 import classes from './main-header.module.css';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
   return (
@@ -18,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
